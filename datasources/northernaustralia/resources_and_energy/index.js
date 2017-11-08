8a086c6f9e0b1f6f23a69cb1f7965f4ac0fcaa8f
Stephen Davies <steve9164@gmail.com>
2017-11-08 16:52:47 +1100

    Rearrange R&E

--- datasources/northernaustralia/resources_and_energy/index.js
+++ datasources/northernaustralia/resources_and_energy/index.js
@@ -12,35 +12,8 @@ const aremi = require('./aremi.json');
 // Remove ids from aremi catalog (screws up when an item is in multiple places in the catalog)
 aremi.catalog = aremi.catalog.map(removeIds);
 
-function importExistingProjects() {
-  const existingProjectsItem = name => getFromCatalogPath(nainvest, ['Existing projects', 'Mines', name]);
-  return [
-    {
-      name: 'Mines and Mining Activity',
-      type: 'group',
-      items: ['Mine Areas'].map(existingProjectsItem).concat([{
-        name: 'Mines - Geoscience Australia',
-        type: 'wms',
-        url: 'http://services.ga.gov.au/earthresource/ows',
-        layers: 'erl:MineView'
-      },{
-        name: 'Mines - Geoscience Australia',
-        type: 'wms',
-        url: 'http://services.ga.gov.au/earthresource/ows',
-        layers: 'erl:MineView'
-      }])
-    },
-    {
-      name: 'Mining tenements',
-      type: 'group',
-      items: ['WA: Mining tenements', 'NT: Mining tenements'].map(existingProjectsItem)
-    },
-    {
-      name: 'Operating mines',
-      type: 'group',
-      items: ['WA: Mines & Mineral deposits'].map(existingProjectsItem)
-    }
-  ];
+function existingProjectsItem(name) {
+  return getFromCatalogPath(nainvest, ['Existing projects', 'Mines', name]);
 }
 
 function importOilAndGas() {
@@ -84,7 +57,41 @@ module.exports = addDescriptionToGroups({
       type: 'group',
       preserveOrder: true,
       items: [
-        require('../shared/soil'),
+        {
+          name: 'Mineral Occurrences',
+          url: 'http://services.ga.gov.au/earthresource/ama/wms',
+          type: 'wms',
+          dataCustodian: '[Geoscience Australia](http://www.ga.gov.au/)',
+          info: [
+            {
+              name: 'Licence',
+              content: '[Creative Commons Attribution 4.0 International (CC BY 4.0)](http://creativecommons.org/licenses/by/4.0/)'
+            }
+          ],
+          layers:'mineral_occurrences'
+        },
+        {
+          name: 'Mines Operating Status',
+          url: 'http://services.ga.gov.au/earthresource/ama/wms',
+          type: 'wms',
+          dataCustodian: '[Geoscience Australia](http://www.ga.gov.au/)',
+          info: [
+            {
+              name: 'Licence',
+              content: '[Creative Commons Attribution 4.0 International (CC BY 4.0)](http://creativecommons.org/licenses/by/4.0/)'
+            }
+          ],
+          layers:'mineral_deposits'
+        },
+        {
+          name: 'Mines and Mineral Deposits',
+          type: 'csv',
+          url: 'data/Resources_and_Energy/mineral_deposits.csv',
+          tableStyle: {
+            dataVariable: 'COMMODIDS'
+          }
+        },
+        ...['WA: Mines & Mineral deposits', 'WA: Mining tenements', 'NT: Mining tenements'].map(existingProjectsItem),
         {
           name: 'Surface Geology',
           type: 'wms-getCapabilities',
@@ -97,31 +104,8 @@ module.exports = addDescriptionToGroups({
             }
           ]
         },
-        {
-          name: 'Mineral Occurrences and Resources',
-          url: 'http://services.ga.gov.au/earthresource/ama/wms',
-          type: 'wms-getCapabilities',
-          dataCustodian: '[Geoscience Australia](http://www.ga.gov.au/)',
-          info: [
-            {
-              name: 'Licence',
-              content: '[Creative Commons Attribution 4.0 International (CC BY 4.0)](http://creativecommons.org/licenses/by/4.0/)'
-            }
-          ],
-          blacklist: {'Major Resource Projects and Infrastructure': true}
-        },
-        {
-          name: 'Mineral Deposits',
-          type: 'csv',
-          url: 'data/Resources_and_Energy/mineral_deposits.csv',
-          tableStyle: {
-            dataVariable: 'COMMODIDS'
-          }
-        },
-        ...importExistingProjects(),
       ]
     },
-    importOilAndGas(),
     importRenewables(getFromCatalogPath(aremi, ['Renewable Energy'])),
     {
       name: 'Infrastructure',
@@ -131,6 +115,8 @@ module.exports = addDescriptionToGroups({
         require('../shared/transport')
       ]
     },
+    importOilAndGas(),
+    require('../shared/soil'),
     {
       name: 'Demography',
       type: 'group',
