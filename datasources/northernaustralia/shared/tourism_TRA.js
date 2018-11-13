97758793e6222e10bfa998eb46d2153c0b7f3ee9
Stephen Davies <steve9164@gmail.com>
2018-11-13 17:48:32 +1100

    Add tourism region origin statistics

--- datasources/northernaustralia/shared/tourism_TRA.js
+++ datasources/northernaustralia/shared/tourism_TRA.js
@@ -11,6 +11,30 @@ module.exports = {
       tableStyle: {
         replaceWithNullValues: ["np"]
       }
+    },
+    {
+      name: "International Visitors by Origin Country ('000)",
+      type: "csv",
+      url: "datasets/2013-2017InternationalTourismOrigins.csv",
+      tableStyle: {
+        replaceWithNullValues: ["np"]
+      }
+    },
+    {
+      name: "Domestic Overnight Visitors by Origin State ('000)",
+      type: "csv",
+      url: "datasets/2013-2017OvernightTourismOrigins.csv",
+      tableStyle: {
+        replaceWithNullValues: ["np"]
+      }
+    },
+    {
+      name: "Domestic Daytrip Visitors by Origin State ('000)",
+      type: "csv",
+      url: "datasets/2013-2017DaytripTourismOrigins.csv",
+      tableStyle: {
+        replaceWithNullValues: ["np"]
+      }
     }
   ]
 };
