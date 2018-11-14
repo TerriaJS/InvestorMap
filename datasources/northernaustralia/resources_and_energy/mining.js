896f7e58e4767cd1e853b9f79d93ea95aa54fa27
Stephen Davies <steve9164@gmail.com>
2018-11-14 18:42:17 +1100

    Update mineral deposits csv and use COMMODNAMES instead of COMMODIDS by default

--- datasources/northernaustralia/resources_and_energy/mining.js
+++ datasources/northernaustralia/resources_and_energy/mining.js
@@ -48,9 +48,11 @@ module.exports = {
       type: "csv",
       url: "data/Resources_and_Energy/mineral_deposits.csv",
       tableStyle: {
-        dataVariable: "COMMODIDS",
+        dataVariable: "COMMODNAMES",
         columns: {
           id: { type: "HIDDEN" },
+          X: { type: "HIDDEN" },
+          Y: { type: "HIDDEN" },
           ENO: { type: "HIDDEN" },
           NAME: { type: "HIDDEN" },
           SYNONYMS: { type: "HIDDEN" },
