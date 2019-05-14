cd245b9526494c28c3fb74359d67ed5fb76fdc2f
Regina Pramesti <regina.pramesti.8@gmail.com>
2019-05-14 16:25:08 +1000

    Add electoral boundaries layers

--- datasources/investormap/root.js
+++ datasources/investormap/root.js
@@ -20,6 +20,7 @@ module.exports = {
         require("./shared/soil"),
         require("./shared/satellite_imagery"),
         require("./shared/climate"),
+        require("./shared/boundaries"),
         ...require("./shared/misc.js"),
         require("./abs")
       ]
